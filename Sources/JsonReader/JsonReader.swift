200a825bfa0eb48b536f9286c8a15ad4e579d4cc
Яков Панов <diegoling33@yandex.ru>
2021-04-08 01:22:26 +0300

    Initial

--- Sources/JsonReader/JsonReader.swift
+++ Sources/JsonReader/JsonReader.swift
@@ -1,3 +1,23 @@
-struct JsonReader {
-    var text = "Hello, World!"
+import Foundation
+
+/**
+ JSON reader tool
+ */
+class JsonReader {
+
+    /**
+     Reads the JSON file
+     - Parameters:
+       - type: the type of file
+       - path: the path of file
+     - Returns: data content
+     - Throws: reading error
+     */
+    static func read<T>(_ type: T.Type, fromFile path: String) throws -> T where T: Decodable {
+        let rawString = try String(contentsOfFile: path, encoding: .utf8)
+        let data = rawString.data(using: .utf8)
+        return try JSONDecoder().decode(type, from: data!)
+    }
+
 }
+
